8df7afd644f8b0a56fdd7defc57cb4dd81f745a3
Jose <Jrivera6869@yahoo.com>
2020-03-24 14:07:36 -0400

    changes

--- api/routes/auth/auth-route.js
+++ api/routes/auth/auth-route.js
@@ -5,6 +5,7 @@ const { jwtSecret } = require('../../config/secrets');
 const router = express.Router();
 const Users = require('../users/user-model');
 const Joi = require('@hapi/joi');
+const axios = require('axios');
 
 const schema = Joi.object({
   email: Joi.string()
@@ -16,21 +17,49 @@ const schema = Joi.object({
     .max(30)
 });
 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
   let user = req.body;
 
   let newuser = schema.validate(user).value;
   if (!schema.validate(user).error) {
-    const hash = bcrypt.hashSync(newuser.password, 10);
-    newuser.password = hash;
+    try {
+      const hash = bcrypt.hashSync(newuser.password, 10);
+      newuser.password = hash;
 
-    Users.add(newuser)
-      .then(saved => {
-        res.status(201).json(saved);
-      })
-      .catch(error => {
-        res.status(500).json(error.message);
+      let saved = await Users.add(newuser);
+      res.status(201).json(saved);
+
+      // let ax = await axios.post(
+      //   'https://dev-966011.okta.com/api/v1/users?activate=true',
+      //   {
+      //     profile: {
+      //       email: email,
+      //       login: email
+      //     }
+      //   },
+      //   {
+      //     headers: {
+      //       Authorization: '007i8tncM4Z-bN6fiP6fvu0AbKS-tvql3lFtxy-6vd'
+      //     }
+      //   }
+      // );
+      // console.log(ax, 'testing');
+    } catch (error) {
+      res.status(500).json({
+        message: error.message,
+        error: error.stack,
+        name: error.name,
+        code: error.code
       });
+    }
+
+    // Users.add(newuser)
+    //   .then(saved => {
+    //     res.status(201).json(saved);
+    //   })
+    //   .catch(error => {
+    //     res.status(500).json(error.message);
+    //   });
   } else {
     res.status(500).json(schema.validate(user).error);
   }
@@ -43,12 +72,20 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        const token = generateToken(user);
-
-        res.status(200).json({
-          message: 'Login successful',
-          token
-        });
+        try {
+          const token = generateToken(user);
+          res.status(200).json({
+            message: 'Login successful',
+            token
+          });
+        } catch (error) {
+          res.status(500).json({
+            message: error.message,
+            error: error.stack,
+            name: error.name,
+            code: error.code
+          });
+        }
       } else {
         res.status(500).json({ error: 'login error' });
       }
