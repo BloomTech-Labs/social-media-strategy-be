536f422918f0e499c3f9ddab4a665be6a34efa50
Jose <Jrivera6869@yahoo.com>
2020-04-04 20:14:20 -0400

    cant get u 2 work

--- knexfile.js
+++ knexfile.js
@@ -4,18 +4,25 @@ require('dotenv').config();
 module.exports = {
   development: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    // connection: process.env.DATABASE_URL,
+    connection: {
+      user: 'jrivera6869',
+      password: 'postgres',
+      database: 'ebdb',
+      host: 'aa1xxezbq1ezyd0.cst1ktu0kueb.us-east-1.rds.amazonaws.com',
+      port: 5432,
+    },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
       directory: './data/migrations',
-      tableName: 'knex_migrations'
+      tableName: 'knex_migrations',
     },
     seeds: {
-      directory: './data/seeds'
-    }
+      directory: './data/seeds',
+    },
   },
 
   testing: {
@@ -24,27 +31,34 @@ module.exports = {
 
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
       directory: './data/migrations',
-      tableName: 'knex_migrations'
-    }
+      tableName: 'knex_migrations',
+    },
   },
 
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL,
+    // connection: process.env.DATABASE_URL,
+    connection: {
+      user: 'jrivera6869',
+      password: 'postgres',
+      database: 'ebdb',
+      host: 'aa1xxezbq1ezyd0.cst1ktu0kueb.us-east-1.rds.amazonaws.com',
+      port: 5432,
+    },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
       directory: './data/migrations',
-      tableName: 'knex_migrations'
+      tableName: 'knex_migrations',
     },
     seeds: {
-      directory: './data/seeds'
-    }
-  }
+      directory: './data/seeds',
+    },
+  },
 };
