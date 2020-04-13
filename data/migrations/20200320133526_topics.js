fc4239f14105dec3de5fbaa5bbcaf20663f03a4c
Jose Rivera <jrivera6869@gmail.com>
2020-04-13 19:52:32 -0400

    changed TOpic id to text

--- data/migrations/20200320133526_topics.js
+++ data/migrations/20200320133526_topics.js
@@ -1,25 +1,22 @@
-exports.up = function(knex) {
-  return knex.schema.createTable('topics', tbl => {
-    tbl.increments('id');
+exports.up = function (knex) {
+  return knex.schema.createTable("topics", (tbl) => {
+    tbl.text("id").primary();
 
-    tbl.text('name', 64).notNullable();
+    tbl.text("name", 64).notNullable();
 
     tbl
-      .integer('user_id')
+      .integer("user_id")
       .unsigned()
       .notNullable()
-      .references('id')
-      .inTable('users')
-      .onUpdate('CASCADE')
-      .onDelete('CASCADE');
+      .references("id")
+      .inTable("users")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
 
-      tbl.specificType('cards', 'text ARRAY');
-
+    tbl.specificType("cards", "text ARRAY");
   });
 };
 
-exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('topics');
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("topics");
 };
-
-
