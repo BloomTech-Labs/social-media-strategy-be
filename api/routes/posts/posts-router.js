8825d3c029068c4cb0a9353e7be1828e3fe11fbd
Jose Rivera <jrivera6869@gmail.com>
2020-04-15 16:30:54 -0400

    testing error

--- api/routes/posts/posts-router.js
+++ api/routes/posts/posts-router.js
@@ -1,69 +1,69 @@
-const express = require("express");
-const Posts = require("./posts-model.js");
-const Joi = require("@hapi/joi");
+const express = require('express');
+const Posts = require('./posts-model.js');
+const Joi = require('@hapi/joi');
 const router = express.Router();
-const axios = require("axios");
-const validate = require("../auth/middleware");
-require("dotenv").config();
+const axios = require('axios');
+const validate = require('../auth/middleware');
+require('dotenv').config();
 
 const schema = Joi.object({
   user_id: Joi.number(),
   platform_id: Joi.number(),
   post_text: Joi.string().required(),
   datestamp: Joi.any(),
-  screenname: Joi.string().allow(""),
+  screenname: Joi.string().allow(''),
 });
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   Posts.find()
     .then((posts) => {
       res.status(200).json({ Posts: posts });
     })
     .catch((err) => {
       res.status(500).json({
-        message: "Error retrieving posts",
+        message: 'Error retrieving posts',
         Error: err,
       });
     });
 });
 
-router.get("/:id", (req, res) => {
+router.get('/:id', (req, res) => {
   const { id } = req.params;
 
   Posts.find({ id })
     .then((posts) => {
-      res.status(200).json({ "Post with specified ID": posts });
+      res.status(200).json({ 'Post with specified ID': posts });
     })
     .catch((err) => {
       res.status(404).json({
-        message: "Post with specified ID not found",
+        message: 'Post with specified ID not found',
         Error: err,
       });
     });
 });
 
-router.get("/:id/user", (req, res) => {
+router.get('/:id/user', (req, res) => {
   const { id } = req.params;
 
   Posts.find({ user_id: id })
     .then((posts) => {
-      res.status(200).json({ "Post by specified user": posts });
+      res.status(200).json({ 'Post by specified user': posts });
     })
     .catch((err) => {
       res.status(404).json({
-        message: "Post with specified ID not found",
+        message: 'Post with specified ID not found',
         Error: err,
       });
     });
 });
 
-router.post("/:id/user", validate.validateuserid, async (req, res) => {
+router.post('/:id/user', validate.validateuserid, async (req, res) => {
   const { okta_userid } = req.decodedToken;
   const { id } = req.params;
-  console.log("HELLO TESTING IF I MAKE IT", id, okta_userid);
+  console.log('HELLO TESTING IF I MAKE IT', id, okta_userid);
 
   try {
-    let ax = await axios.get(
+    let axx = await axios.get(
       `https://${process.env.OKTA_DOMAIN}/users/${okta_userid}`,
       {
         headers: {
@@ -74,7 +74,7 @@ router.post("/:id/user", validate.validateuserid, async (req, res) => {
   } catch (error) {
     console.log(error);
   }
-  console.log(ax.data.twitter_screenName, "SCREENNAME FROM OKTA");
+  console.log(ax.data.twitter_screenName, 'SCREENNAME FROM OKTA');
   const postbody = {
     ...req.body,
     screenname: ax.data.twitter_screenName,
@@ -86,11 +86,11 @@ router.post("/:id/user", validate.validateuserid, async (req, res) => {
     try {
       let post = await Posts.add(postbody);
       let ax = await axios.post(
-        " https://social-media-strategy-ds.herokuapp.com/recommend",
+        ' https://social-media-strategy-ds.herokuapp.com/recommend',
         post
       );
 
-      console.log(post, ax, postbody, "TESTING");
+      console.log(post, ax, postbody, 'TESTING');
       return res.status(201).json(post);
     } catch (error) {
       res.status(500).json({
@@ -103,33 +103,33 @@ router.post("/:id/user", validate.validateuserid, async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
+router.put('/:id', (req, res) => {
   const { id } = req.params;
   const update = req.body;
 
   Posts.update(update, id) //May need to change depending on payload
     .then((value) => {
-      res.status(201).json({ "Updated post: ": value });
+      res.status(201).json({ 'Updated post: ': value });
     })
     .catch((err) => {
       // console.log(err.message)
       res
         .status(500)
-        .json({ message: "Post cannot be updated", Error: err.message });
+        .json({ message: 'Post cannot be updated', Error: err.message });
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete('/:id', (req, res) => {
   const { id } = req.params;
 
   Posts.remove(id)
     .then((response) => {
-      res.status(200).json({ message: "Post deleted", response });
+      res.status(200).json({ message: 'Post deleted', response });
     })
     .catch((err) => {
       res
         .status(500)
-        .json({ message: "Post cannot be removed", Error: err.message });
+        .json({ message: 'Post cannot be removed', Error: err.message });
     });
 });
 
