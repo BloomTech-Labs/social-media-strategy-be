45a513d9aa3cb530bba5dc73ddee4563f3129788
Jose Rivera <jrivera6869@gmail.com>
2020-04-16 23:16:43 -0400

    added stuff

--- api/routes/auth/auth-route.js
+++ api/routes/auth/auth-route.js
@@ -37,6 +37,7 @@ const schema = Joi.object({
 router.get('/:id/oauth', validateuserid, async (req, res, next) => {
   try {
     let twit = await client.getRequestToken('https://so-me.net/callback ');
+    // https://master.duosa5dkjv93b.amplifyapp.com/callback     <-- if so-me in not fixed
 
     const redirecturl = `https://api.twitter.com/oauth/authorize?oauth_token=${twit.oauth_token}`;
 
