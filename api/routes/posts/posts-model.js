dac98f0f93bbf072a5baabbdfa6132a10bdebf01
Jose Rivera <jrivera6869@gmail.com>
2020-04-17 16:21:21 -0400

    updated stuff

--- api/routes/posts/posts-model.js
+++ api/routes/posts/posts-model.js
@@ -5,7 +5,7 @@ module.exports = { find, add, remove, update };
 function find(filter) {
   let posts = db('posts');
   if (filter) {
-    return posts.where(filter);
+    return posts.where({ filter });
   } else {
     return posts;
   }
@@ -21,12 +21,12 @@ function remove(id) {
   return db('posts')
     .where({ id })
     .del()
-    .then(res => find());
+    .then((res) => find());
 }
 
 function update(post, id) {
   return db('posts')
     .where('id', id)
     .update(post)
-    .then(updated => (updated > 0 ? find({ id }) : null));
+    .then((updated) => (updated > 0 ? find({ id }) : null));
 }
