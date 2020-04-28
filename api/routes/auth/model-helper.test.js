5cbd01412588dd29d3c92c46f0ab300375762efc
Tyler Gilchrist <tyler668@gmail.com>
2020-04-28 15:11:54 -0400

    Added more dynamic tests

--- api/routes/auth/model-helper.test.js
+++ api/routes/auth/model-helper.test.js
@@ -1,5 +1,5 @@
 const db = require('../../../data/db.config');
-const Posts = require('../posts/posts-model.js');
+// const Posts = require('../posts/posts-model.js');
 const Topics = require('../topics/topics-model.js')
 const [
   joivalidation,
@@ -9,58 +9,112 @@ const [
   find,
   add,
   ModelRemove,
-  UserUpdate,
+  ModelUpdate,
+  findByID
 ] = require('../../helper');
 
 const postExample = {
-    id: 4,
-    user_id: 1,
-    post_text: 'Hello Saturn!',
-    completed: false,
-    date: null,
-    tz: null,
-    optimal_time: null,
-    post_score: null,
-  }
+  id: 4,
+  user_id: 1,
+  post_text: 'Hello Saturn!',
+  completed: false,
+  date: null,
+  tz: null,
+  optimal_time: null,
+  post_score: null,
+}
 
-  const topicExample = {
-    id: 1,
-    title: "New Topic1",
-    user_id: 1,
-    cards: []
+const topicExample = {
+  id: 1,
+  title: "New Topic1",
+  user_id: 1,
+  cards: []
 }
 
 const userExample = {
-    id: 50,
-    email: 'hal@hal.com',
-    password: 'test',
-    okta_userid: '12323'
+  id: 50,
+  email: 'hal@hal.com',
+  password: 'test',
+  okta_userid: '12323'
 }
 
-function modelTester(table, model, formattedSubmission){
-    describe('insert function inserts', () => {
-        it('inserts a new item into the db', async () => {
-          await db(`${table}`).truncate();
-          let tableLength;
-          tableLength = await db(`${table}`);
-          expect(tableLength).toHaveLength(0);
-      
-          await add(table, formattedSubmission);
-      
-          tableLength = await db(`${table}`);
-      
-          expect(tableLength).toHaveLength(1);
-      
-          await ModelRemove(table, formattedSubmission.id);
-      
-          tableLength = await db(`${table}`);
-          expect(tableLength).toHaveLength(0);
-        });
-      });   
+const userUpdate = {
+  id: 50,
+  email: 'update@update.com',
+  password: 'test',
+  okta_userid: '12323'
+  
 }
 
-modelTester('posts', Posts, postExample);
-modelTester('topics', Topics, topicExample);
+const postUpdate = {
+  id: 4,
+  user_id: 1,
+  post_text: "Updated text",
+  completed: false,
+  date: null,
+  tz: null,
+  optimal_time: null,
+  post_score: null,
+  screenname:null
+}
+
+const topicUpdate = {
+  id: "1",
+  title: "Updated Topic",
+  user_id: 1,
+  cards: [],
+  index: null
+}
+
+function addRemoveTester(table, formattedSubmission) {
+  describe('insert function inserts', () => {
+    it('inserts a new item into the db', async () => {
+      await db(`${table}`).truncate();
+      let tableLength;
+      tableLength = await db(`${table}`);
+      expect(tableLength).toHaveLength(0);
+
+      await add(table, formattedSubmission);
+
+      tableLength = await db(`${table}`);
+
+      expect(tableLength).toHaveLength(1);
+
+      await ModelRemove(table, formattedSubmission.id);
+
+      tableLength = await db(`${table}`);
+      expect(tableLength).toHaveLength(0);
+    });
+  });
+}
+
+function updateTester(table, formattedSubmission, formattedUpdate) {
+  describe('Updates inserted values for users and posts tables', () => {
+    it('inserts a new item into the db', async () => {
+      await db(`${table}`).truncate();
+      let tableLength;
+      tableLength = await db(`${table}`);
+      expect(tableLength).toHaveLength(0);
+
+      await add(table, formattedSubmission);
+
+      tableLength = await db(`${table}`);
+
+      expect(tableLength).toHaveLength(1);
+
+      await ModelUpdate(table, formattedUpdate, formattedSubmission.id);
+
+      tableLength = await db(`${table}`);
+      //console.log('HEREHEHEHRHERE', tableLength[0]);
+      expect(tableLength[0]).toEqual(formattedUpdate);
+    });
+  });
+}
+
+addRemoveTester('posts', postExample);
+addRemoveTester('topics', topicExample);
+updateTester('posts', postExample, postUpdate);
+updateTester('topics', topicExample, topicUpdate);
 // modelTester('users', Users, userExample);
 
 
