f0c1b4dd6c79cb4d05e9e446641f97621e8fd566
jackskim <jackskim29@gmail.com>
2020-05-14 16:23:53 -0700

    Cleanup migrations

--- data/migrations/20200318175517_users.js
+++ data/migrations/20200318175517_users.js
@@ -1,19 +1,13 @@
-exports.up = function(knex) {
-  return knex.schema.createTable('users', tbl => {
-    tbl.increments('id');
-    tbl
-      .text('email')
-      .notNullable()
-      .unique();
-    tbl.text('password').notNullable();
-    tbl.text('okta_userid').notNullable();
+exports.up = function (knex) {
+  return knex.schema.createTable("users", (tbl) => {
+    tbl.increments("id");
+    tbl.text("email").notNullable().unique();
+    tbl.text("password").notNullable();
+    tbl.text("okta_userid").notNullable();
+    tbl.text("role");
   });
 };
 
-exports.down = function(knex) {
-  return knex.schema
-    .dropTableIfExists('posts')
-    .dropTableIfExists('topics')
-    .dropTableIfExists('platforms')
-    .dropTableIfExists('users');
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("users");
 };
--- data/migrations/20200320133526_topics.js
+++ data/migrations/20200320133526_topics.js
@@ -2,7 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable("topics", (tbl) => {
     tbl.text("id").primary();
 
-    tbl.text("name", 64).notNullable();
+    tbl.text("title", 64).notNullable();
 
     tbl
       .integer("user_id")
@@ -13,7 +13,9 @@ exports.up = function (knex) {
       .onUpdate("CASCADE")
       .onDelete("CASCADE");
 
-    tbl.specificType("cards", "text ARRAY");
+    tbl.specificType("cards", "text ARRAY").defaultTo("{}");
+
+    tbl.integer("index");
   });
 };
 
