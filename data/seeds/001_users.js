131cb61b44d9fc955af905fd86eed3655204ba16
jackskim <jackskim29@gmail.com>
2020-05-18 09:07:39 -0700

    Remove unhashed pw comments

--- data/seeds/001_users.js
+++ data/seeds/001_users.js
@@ -7,14 +7,12 @@ exports.seed = function (knex) {
       return knex("users").insert([
         {
           email: "some@test.com",
-          // unhashed password : some
           password:
             "$2a$10$RLMiGvYEo8DLIOMS7CXPQ.GjJnHzO91MQFwVAndcUQZvqGwZ5HN/a",
           okta_userid: "00uc5g9hhHet4fOqH4x6",
         },
         {
           email: "jack@test.com",
-          // unhashed password: test
           password:
             "$2a$10$KgZCZogsKpAB4sQWTtq6KOSh6bRfRAt1TsG6g.hAI0NObh4qpqKQ2",
           okta_userid: "00uc5m8mocgI0NbKG4x6",
