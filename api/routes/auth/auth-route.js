35af050b289497c3877b381bd4e3651e6e2c0993
Tim Shaker <tim@tmshkr.com>
2020-05-28 19:47:47 +0000

    Update auth-route.js
    Co-authored-by: M Groesback <m.groesbeck@gmail.com>
    Co-authored-by: Tim Shaker <tim@tmshk.com>
    Co-authored-by: Jack Kim <jackskim29@gmail.com>
    Co-authored-by: Eddie Madrigal <edmadrigal@yahoo.com
    Co-authored-by: Fabricio Bezerra <fabricio.spb@gmail.com>
    Co-authored-by: Aja Blanco <ajakblanco@gmail.com>

--- api/routes/auth/auth-route.js
+++ api/routes/auth/auth-route.js
@@ -4,10 +4,8 @@ const router = express.Router();
 const axios = require("axios");
 const Twitter = require("twitter-lite");
 const { twitterInfo } = require("../auth/middleware");
-const restricted = require("../auth/restricted-middleware");
 const verifyJWT = require("./okta-jwt-verifier");
 const queryString = require("query-string");
-var Twit = require("twit");
 
 const client = new Twitter({
   consumer_key: process.env.CONSUMER_KEY,
@@ -71,30 +69,6 @@ router.post("/twitter/callback", verifyJWT, async (req, res, next) => {
       console.error(err);
       next({ code: 500, message: "There was a problem saving your data" });
     });
-
-  //   var T = new Twit({
-  //     consumer_key: process.env.CONSUMER_KEY,
-  //     consumer_secret: process.env.CONSUMER_SECRET,
-  //     access_token: parsed_data.oauth_token,
-  //     access_token_secret: parsed_data.oauth_token_secret,
-  //   });
-  //   let a = await T.get("followers/ids", {
-  //     screen_name: `${parsed_data.screen_name}`,
-  //   });
-  //   let totalfollowers = await a.data.ids.length;
-
-  //   res.status(200).json({
-  //     twitter_screenName: parsed_data.screen_name,
-  //     total_followers: totalfollowers,
-  //   });
-  // } catch (error) {
-  //   res.status(500).json({
-  //     message: error.message,
-  //     error: error.stack,
-  //     name: error.name,
-  //     code: error.code,
-  //   });
-  // }
 });
 
 router.get("/twitter/disconnect", verifyJWT, async (req, res, next) => {
@@ -130,40 +104,6 @@ router.get("/twitter/disconnect", verifyJWT, async (req, res, next) => {
         message: "There was a problem disconnecting your account",
       });
     });
-});
-
-router.get("/userInfo", restricted, twitterInfo, async (req, res) => {
-  try {
-    let twitInfo = await req.twit.get("users/show", {
-      screen_name: `${req.okta.data.profile.twitter_screenName}`,
-    });
-
-    res.status(200).json({
-      screen_name: req.okta.data.profile.twitter_screenName,
-      total_followers: twitInfo.data.followers_count,
-      total_following: twitInfo.data.friends_count,
-      total_post: twitInfo.data.statuses_count,
-      profile_img: twitInfo.data.profile_image_url_https,
-      location: twitInfo.data.location,
-      name: twitInfo.data.name,
-    });
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-router.get("/userStream", restricted, twitterInfo, async (req, res) => {
-  console.log("SCREENY", req.okta.data.profile.twitter_screenName);
-  try {
-    let twitInfo = await req.twit.get("statuses/home_timeline/:count", {
-      screen_name: `${req.okta.data.profile.twitter_screenName}`,
-      count: 1,
-    });
-
-    res.status(200).json({ stream_data: twitInfo.data });
-  } catch (error) {
-    console.log(error);
-  }
 });
 
 module.exports = router;
