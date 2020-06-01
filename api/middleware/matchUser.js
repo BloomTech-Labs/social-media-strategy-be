08211b981a2b2b91d158f63b282528661fb72493
jackskim <jackskim29@gmail.com>
2020-06-01 13:27:59 -0700

    Remove unused exports

--- api/middleware/matchUser.js
+++ api/middleware/matchUser.js
@@ -2,7 +2,7 @@ const db = require("../../data/dbConfig");
 const Lists = require("../models/listModel");
 const Posts = require("../models/postsModel");
 
-module.exports = { matchListUser, matchPostUser, matchUser };
+module.exports = { matchUser };
 
 function matchUser(db_table) {
   return async (req, res, next) => {
