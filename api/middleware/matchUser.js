5e346c847575b12800f77a5198a2c7bf715a36c0
M Groesbeck <m.groesbeck@gmail.com>
2020-06-01 06:14:53 -0700

    Added alternate version of matchUser middleware.

--- api/middleware/matchUser.js
+++ api/middleware/matchUser.js
@@ -1,22 +1,40 @@
+const db = require("../../data/dbConfig");
 const Lists = require("../models/listModel");
 const Posts = require("../models/postsModel");
 
 module.exports = { matchListUser, matchPostUser, matchUser };
 
 async function matchListUser(req, res, next) {
-    const [thisList] = await Lists.find({id:req.params.id});
-    if (thisList.okta_uid === req.jwt.claims.uid){
-        next();
-    } else {
-        res.status(403).json({ message: "Logged in user does not match resource owner." });
-    }
+  const [thisList] = await Lists.find({ id: req.params.id });
+  if (thisList.okta_uid === req.jwt.claims.uid) {
+    next();
+  } else {
+    res
+      .status(403)
+      .json({ message: "Logged in user does not match resource owner." });
+  }
 }
 
 async function matchPostUser(req, res, next) {
-    const [thisPost] = await Posts.find({id:req.params.id});
-    if (thisPost.okta_uid === req.jwt.claims.uid){
-        next();
+  const [thisPost] = await Posts.find({ id: req.params.id });
+  if (thisPost.okta_uid === req.jwt.claims.uid) {
+    next();
+  } else {
+    res
+      .status(403)
+      .json({ message: "Logged in user does not match resource owner." });
+  }
+}
+
+function matchUser(db_table) {
+  return async (req, res, next) => {
+    const [thisItem] = await db(db_table).where({ id: req.params.id });
+    if (thisItem.okta_uid === req.jwt.claims.uid) {
+      next();
     } else {
-        res.status(403).json({ message: "Logged in user does not match resource owner." });
+      res
+        .status(403)
+        .json({ message: "Logged in user does not match resource owner." });
     }
+  };
 }
