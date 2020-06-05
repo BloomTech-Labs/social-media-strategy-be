6cae8a89b3456608be92a134412539c2ee52ed75
Eddie Madrigal <edmadrigal@yahoo.com>
2020-06-05 13:41:09 -0700

    comments added

--- api/routers/__tests__/userRouter.js
+++ api/routers/__tests__/userRouter.js
@@ -1,10 +1,21 @@
+const db = require("../../../data/dbConfig");
 const userRouter = require('../userRouter');
 
 const userRouterTesting = endpoint => {
-    // test for general functionality
-    test('true to be true', () => {
-        expect(true).toBe(true);
-    })
+    describe('Users model', () => {
+
+        beforeEach(async () => {
+            await db('users')
+                .delete() 
+        });
+        // test for general functionality
+        test('true to be true', () => {
+            expect(true).toBe(true);
+        })
+        test('true to NOT be false', () => {
+            expect(true).not.toBe(false);
+        })
+});
 };
 
 userRouterTesting();
