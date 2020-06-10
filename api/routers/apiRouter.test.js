b370b41376791d888669dbabbf6cb7869321df77
Tim Shaker <tim@tmshkr.com>
2020-06-10 13:12:28 -0700

    test GET /api

---
+++ api/routers/apiRouter.test.js
@@ -0,0 +1,12 @@
+const request = require("supertest");
+const server = require("../server.js");
+
+describe("GET /api", () => {
+  it("returns 200", () => {
+    return request(server)
+      .get("/api")
+      .then((res) => {
+        expect(res.status).toBe(200);
+      });
+  });
+});
