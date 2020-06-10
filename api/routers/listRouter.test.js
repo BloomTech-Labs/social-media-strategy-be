c2ba2877962761110a45173ad19102c1a713a0dd
Tim Shaker <tim@tmshkr.com>
2020-06-10 14:07:21 -0700

    run seeds before test

--- api/routers/listRouter.test.js
+++ api/routers/listRouter.test.js
@@ -1,11 +1,15 @@
 const request = require("supertest");
 const server = require("../server.js");
+const db = require("../../data/dbConfig");
+
+beforeAll(() => db.seed.run());
 
 describe("GET /api/lists", () => {
   it("returns 200", () => {
     return request(server)
       .get("/api/lists/")
       .then((res) => {
+        console.log(res.body);
         expect(res.status).toBe(200);
       });
   });
