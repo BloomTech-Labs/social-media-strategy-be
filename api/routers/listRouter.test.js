c791e5e2b3bbc76824eaf02970ab22b8a8da4dcc
Tim Shaker <tim@tmshkr.com>
2020-06-11 09:27:54 -0700

    test GET /api/lists/:id/posts

--- api/routers/listRouter.test.js
+++ api/routers/listRouter.test.js
@@ -66,3 +66,45 @@ describe("GET /api/lists/:id", () => {
       });
   });
 });
+
+describe("GET /api/lists/:id/posts", () => {
+  it("returns 200 for a list that exists belonging to the logged in user", () => {
+    return request(server)
+      .get("/api/lists/fc85a964-eec3-42eb-a076-4d7d2634b321/posts")
+      .then((res) => {
+        expect(res.status).toBe(200);
+      });
+  });
+
+  it("returns an array", () => {
+    return request(server)
+      .get("/api/lists/fc85a964-eec3-42eb-a076-4d7d2634b321/posts")
+      .then((res) => {
+        expect(Array.isArray(res.body)).toBe(true);
+      });
+  });
+
+  it("returns the correct number of posts", () => {
+    return request(server)
+      .get("/api/lists/fc85a964-eec3-42eb-a076-4d7d2634b321/posts")
+      .then((res) => {
+        expect(res.body.length).toBe(3);
+      });
+  });
+
+  it("returns 404 when trying to access another user's list", () => {
+    return request(server)
+      .get("/api/lists/013e4ab9-77e0-48de-9efe-4d96542e791f")
+      .then((res) => {
+        expect(res.status).toBe(404);
+      });
+  });
+
+  it("returns 500 for an invalid id", () => {
+    return request(server)
+      .get("/api/lists/foo")
+      .then((res) => {
+        expect(res.status).toBe(500);
+      });
+  });
+});
