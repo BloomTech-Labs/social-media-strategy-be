b98541da9807c4f5d2fc25d659e95a153ebb19b8
Tim Shaker <tim@tmshkr.com>
2020-06-11 12:50:40 -0700

    test PUT /api/posts/:id/postnow

--- api/routers/postRouter.test.js
+++ api/routers/postRouter.test.js
@@ -66,3 +66,29 @@ describe("GET /api/posts/:id", () => {
       });
   });
 });
+
+describe("PUT /api/posts/:id/postnow", () => {
+  it("returns 200", () => {
+    return request(server)
+      .put("/api/posts/634b0e5c-af78-425b-8ad6-4622986e2e0f/postnow")
+      .then((res) => {
+        expect(res.status).toBe(200);
+      });
+  });
+
+  it("returns returns the posted tweet", () => {
+    return request(server)
+      .put("/api/posts/b46052ea-ba9b-48c7-8f31-3c4d9930b596/postnow")
+      .then((res) => {
+        expect(res.body.posted).toBe(true);
+      });
+  });
+
+  it("returns 404 when trying to tweet a post not belonging to logged in user", () => {
+    return request(server)
+      .put("/api/posts/41670103-3eba-4cb3-8d17-8fd79d9d3dfa/postnow")
+      .then((res) => {
+        expect(res.status).toBe(404);
+      });
+  });
+});
