fc20d7bfb2d26f2755d46bf779c6b7023c1eb8a9
Tim Shaker <tim@tmshkr.com>
2020-06-12 14:47:31 -0700

    add postgres test db

--- knexfile.js
+++ knexfile.js
@@ -27,9 +27,9 @@ module.exports = {
     client: "pg",
     connection: {
       host: "localhost",
-      database: "some-test",
-      user: process.env.PG_USERNAME,
-      password: process.env.PG_PASSWORD,
+      database: process.env.PG_TEST_DB || "postgres",
+      user: process.env.PG_USERNAME || "postgres",
+      password: process.env.PG_PASSWORD || "postgres",
     },
 
     pool: {
