2677469ce313be3df0eea2f1d573ea09d7bf31e2
Tim Shaker <tim@tmshkr.com>
2020-06-15 13:52:44 -0700

    update test GET /api

--- api/routers/__tests__/apiRouter.test.js
+++ api/routers/__tests__/apiRouter.test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const server = require("../../server.js");
 
 describe("GET /api", () => {
-  it("returns 200", async () => {
-    await request(server).get("/api").expect(200);
+  it("returns 200", (done) => {
+    request(server).get("/api").expect(200, done);
   });
 });
