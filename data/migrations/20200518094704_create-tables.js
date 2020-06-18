35d2fde2c756a5cfb294b84b60378e90a7aeeeba
fbzr <fabricio.spb@gmail.com>
2020-06-18 09:11:53 -0700

    post update - save scheduled_time as timestamp and create image_url column

--- data/migrations/20200518094704_create-tables.js
+++ data/migrations/20200518094704_create-tables.js
@@ -1,44 +1,45 @@
 exports.up = function (knex) {
-  return knex.schema
-    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
-    .createTable("lists", (tbl) => {
-      tbl
-        .uuid("id")
-        .notNullable()
-        .unique()
-        .primary()
-        .defaultTo(knex.raw("uuid_generate_v4()"));
-      tbl.string("okta_uid").notNullable();
-      tbl.timestamp("created_at").defaultTo(knex.fn.now());
-      tbl.integer("index").notNullable();
-      tbl.string("title").notNullable();
-    })
-    .createTable("posts", (tbl) => {
-      tbl
-        .uuid("id")
-        .notNullable()
-        .unique()
-        .primary()
-        .defaultTo(knex.raw("uuid_generate_v4()"));
-      tbl.string("okta_uid").notNullable();
-      tbl
-        .uuid("list_id")
-        .notNullable()
-        .references("lists.id")
-        .onUpdate("CASCADE")
-        .onDelete("CASCADE");
-      tbl.timestamp("created_at").defaultTo(knex.fn.now());
-      tbl.integer("index").notNullable();
-      tbl.text("post_text");
-      tbl.boolean("posted").notNullable().defaultsTo(false);
-      tbl.integer("optimal_time");
-      tbl.integer("scheduled_time");
-    });
+	return knex.schema
+		.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+		.createTable("lists", (tbl) => {
+			tbl
+				.uuid("id")
+				.notNullable()
+				.unique()
+				.primary()
+				.defaultTo(knex.raw("uuid_generate_v4()"));
+			tbl.string("okta_uid").notNullable();
+			tbl.timestamp("created_at").defaultTo(knex.fn.now());
+			tbl.integer("index").notNullable();
+			tbl.string("title").notNullable();
+		})
+		.createTable("posts", (tbl) => {
+			tbl
+				.uuid("id")
+				.notNullable()
+				.unique()
+				.primary()
+				.defaultTo(knex.raw("uuid_generate_v4()"));
+			tbl.string("okta_uid").notNullable();
+			tbl
+				.uuid("list_id")
+				.notNullable()
+				.references("lists.id")
+				.onUpdate("CASCADE")
+				.onDelete("CASCADE");
+			tbl.timestamp("created_at").defaultTo(knex.fn.now());
+			tbl.integer("index").notNullable();
+			tbl.text("post_text");
+			tbl.boolean("posted").notNullable().defaultsTo(false);
+			tbl.timestamp("optimal_time");
+			tbl.timestamp("scheduled_time");
+			tbl.string("image_url");
+		});
 };
 
 exports.down = function (knex) {
-  return knex.schema
-    .dropTableIfExists("posts")
-    .dropTableIfExists("lists")
-    .dropTableIfExists("users");
+	return knex.schema
+		.dropTableIfExists("posts")
+		.dropTableIfExists("lists")
+		.dropTableIfExists("users");
 };
