9b16df28bda0bfa9a97f257ff17792fd0aad564b
fbzr <fabricio.spb@gmail.com>
2020-06-19 17:04:43 -0700

    change migration - create list_schedule table

--- data/migrations/20200518094704_create-tables.js
+++ data/migrations/20200518094704_create-tables.js
@@ -34,7 +34,22 @@ exports.up = function (knex) {
 			tbl.timestamp("optimal_time");
 			tbl.timestamp("scheduled_time");
 			tbl.string("image_url");
-		});
+		})
+		.createTable("list_schedule", (tbl) => {
+			tbl.uuid("id").primary().defaultTo(knex.raw("uuid_generate_v4()"));
+			tbl
+				.uuid("list_id")
+				.notNullable()
+				.references("lists.id")
+				.onUpdate("CASCADE")
+				.onDelete("CASCADE");
+			tbl.integer("week_day").notNullable();
+			tbl.integer("hour").notNullable();
+			tbl.integer("minute").notNullable();
+		})
+		.raw(
+			"ALTER TABLE list_schedule ADD CONSTRAINT check_schedule CHECK (week_day >= 0 AND week_day <= 6 AND hour >= 0 AND hour <= 23 AND minute >= 0 AND minute <= 59)",
+		);
 };
 
 exports.down = function (knex) {
